2c229fe7749ac281ceadab2a065715320af9d077
Loneth <notloneth@gmail.com>
2021-10-19 02:46:42 +0700

    Initial Commit

--- src/nereus/tasks/MonsterRespawn.java
+++ src/nereus/tasks/MonsterRespawn.java
@@ -8,11 +8,11 @@ public class MonsterRespawn implements Runnable {
    private World world;
 
    public MonsterRespawn(World world, MonsterAI ai) {
-      super();
       this.ai = ai;
       this.world = world;
    }
 
+   @Override
    public void run() {
       this.ai.restore();
       this.world.send(new String[]{"respawnMon", Integer.toString(this.ai.getMapId())}, this.ai.getRoom().getChannellList());
